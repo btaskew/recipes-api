f02e89b239d65921fbafa2c81075fde986725ed2
baskew <b.askew@exeter.ac.uk>
2019-05-10 11:11:12 +0100

    Setup testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,14 +1,54 @@
 <?php
 
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Laravel\Lumen\Application;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
 abstract class TestCase extends Laravel\Lumen\Testing\TestCase
 {
+    use DatabaseMigrations;
+
+    /**
+     * Default exception handler
+     *
+     * @var ExceptionHandler
+     */
+    private $oldExceptionHandler;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->disableExceptionHandling();
+    }
+
     /**
      * Creates the application.
      *
-     * @return \Laravel\Lumen\Application
+     * @return Application
      */
     public function createApplication()
     {
         return require __DIR__.'/../bootstrap/app.php';
+    }
+
+    /**
+     * Disable exception handling by default for better test debugging
+     */
+    private function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct()
+            {
+            }
+            public function report(\Exception $e)
+            {
+            }
+            public function render($request, \Exception $e)
+            {
+                throw $e;
+            }
+        });
     }
 }
